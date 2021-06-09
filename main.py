8958bd92f7c7920eca333fedc8c63dfe676adbbd
trim-dem <d.trimarco1@studenti.unisa.it>
2021-06-09 13:54:02 +0200

    .

--- main.py
+++ main.py
@@ -266,6 +266,12 @@ SEED_VEHICLES          = 0    # seed for vehicle spawn randomizer
 PLAYER_START_INDEX = 15       #  spawn index for player
 DESTINATION_INDEX = 148         # Setting a Destination HERE
 NUM_PEDESTRIANS        = 200     # total number of pedestrians to spawn
+NUM_VEHICLES           = 100   # total number of vehicles to spawn
+SEED_PEDESTRIANS       = 0      # seed for pedestrian spawn randomizer
+SEED_VEHICLES          = 0    # seed for vehicle spawn randomizer
+'''
+
+'''
 ################################## --- #################################################
 PLAYER_START_INDEX = 11       # spawn index for player
 DESTINATION_INDEX = 20         # Setting a Destination HERE
